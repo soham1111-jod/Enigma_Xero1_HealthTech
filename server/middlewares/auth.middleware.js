29c6abae995f3be5e7fcec75640023843ae031f1
soham1111-jod <sohamchafale11@gmail.com>
2025-09-20 12:31:43 +0530

    Enhance JWT verification middleware to handle token expiration and errors

--- server/middlewares/auth.middleware.js
+++ server/middlewares/auth.middleware.js
@@ -1,28 +1,68 @@
 
+// import { asyncHandler } from "../utils/asyncHandler.js";
+// import { ApiError } from "../utils/APIError.js";
+// import jwt from "jsonwebtoken";
+// import { User } from "../models/User.model.js";
+
+// export const verifyJWT = asyncHandler(async (req, res, next) => {
+//   try {
+//     const token = req.cookies?.accessToken;
+  
+//     if (!token) {
+//       throw new ApiError(401, "Unauthorized request");
+//     }
+  
+//     const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+  
+//     const user = await User.findById(decodedToken?._id).select(
+//       "-password -refreshToken"
+//     );
+//     if (!user) {
+//       throw new ApiError(401, "Invalid Token");
+//     }
+//     req.user = user;
+//     next();
+//   } catch (error) {
+//     throw new ApiError(401,"Invalid Access Token")
+//   }
+// });
+
+
+
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/APIError.js";
 import jwt from "jsonwebtoken";
 import { User } from "../models/User.model.js";
 
 export const verifyJWT = asyncHandler(async (req, res, next) => {
+  const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "");
+
+  if (!token) {
+    throw new ApiError(401, "Unauthorized request");
+  }
+
   try {
-    const token = req.cookies?.accessToken;
-  
-    if (!token) {
-      throw new ApiError(401, "Unauthorized request");
-    }
-  
     const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-  
+    
     const user = await User.findById(decodedToken?._id).select(
       "-password -refreshToken"
     );
+    
     if (!user) {
       throw new ApiError(401, "Invalid Token");
     }
+    
     req.user = user;
     next();
   } catch (error) {
-    throw new ApiError(401,"Invalid Access Token")
+    // Handle JWT-specific errors
+    if (error.name === 'JsonWebTokenError') {
+      throw new ApiError(401, "Invalid Access Token");
+    } else if (error.name === 'TokenExpiredError') {
+      throw new ApiError(401, "Access Token Expired");
+    } else {
+      // Re-throw ApiError instances or create new one
+      throw error instanceof ApiError ? error : new ApiError(401, "Token verification failed");
+    }
   }
 });
